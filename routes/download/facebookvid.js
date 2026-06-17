09718f8ad0cb7c9ea3bbbd5498634ccb92c19243
𝙳𝚟𝚆𝚒𝚕𝚔𝚎𝚛𝙾𝙵𝙲 𝚅𝟷 <developer.wilker.ofc@gmail.com>
2026-06-17 00:23:19 -0400

    Actualizar facebookvid.js

--- routes/download/facebookvid.js
+++ routes/download/facebookvid.js
@@ -42,7 +42,7 @@ router.get('/', async (req, res) => {
 
     return res.json({
       status: true,
-      creator: 'Félix Ofc',
+      creator: 'DvWilkerOFC',
       metadata: {
         title: meta.title,
         description: meta.description,
